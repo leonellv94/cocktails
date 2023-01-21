91c0a8af7fdc82b34eb885b42c619f4c28049a89
leonellv94 <112275614+leonellv94@users.noreply.github.com>
2023-01-21 03:27:59 -0300

    Add files via upload

--- temps/js/code.js
+++ temps/js/code.js
@@ -2,17 +2,30 @@
 
 
 
-
+let searchInput = document.querySelector('.searchInput')
+let searchButton = document.querySelector('.searchButton')
 let API_LIST_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
+let API_SEARCH = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const xhr = new XMLHttpRequest();
+const xhr2 = new XMLHttpRequest();
 const HTMLResponse = document.querySelector('#drinks')
 const ingredients = document.querySelector('.ingredients_container')
 const ingredientsQuantity = document.querySelector('.ingredients_quantity_container')
 const links = document.querySelectorAll('.links')
 let drinks = []
 
+////////////////////WINDOWS SCROLL //////////////////
+
+function scrolls() { 
+    window.scroll(0, 0)
+}
+
+////////////////////ALPHABET ORDER //////////////////
+
+
 links.forEach((link) => {
     link.addEventListener('click', () => {
+        scrolls()
         HTMLResponse.innerHTML = '';
         ingredients.innerHTML = '';
         ingredientsQuantity.innerHTML = '';
@@ -22,21 +35,39 @@ links.forEach((link) => {
         arr.push(alphabetLetter)
         API_LIST_URL = arr.join('')
 
+
         xhr.open('GET', `${API_LIST_URL}`)
         xhr.send()
     })
 })
 
+///////////////////////SEARCH INPUT /////////////////
+
+searchButton.addEventListener('click', (e) => {
+    scrolls()
+    HTMLResponse.innerHTML = ''
+    ingredients.innerHTML = '';
+    ingredientsQuantity.innerHTML = '';
+
+    let inputContent = searchInput.value.toLowerCase()
+    API_SEARCH = API_SEARCH.concat(inputContent)
+    console.log(API_SEARCH)
+    xhr.open('GET', `${API_SEARCH}`)
+    xhr.send()
+
+    e.preventDefault()
+})
+
+
+/////////////////////// REQUESTS ////////////////////////
 
 function onRequestHandler() {
     if(this.readyState == 4 && this.status == 200) {
         const data = JSON.parse(this.response);
-        // console.log(data.drinks)
         if(data.drinks === null) {
-            HTMLResponse.innerHTML = '<p style="color:red;">Not Found</p>'
+            HTMLResponse.innerHTML = '<p style="color:red;">Elements not Found</p>'
         }
         for(let i = 0; i < data.drinks.length; i++) {
-            // console.log(data.drinks[i].strIngredient1)
             HTMLResponse.innerHTML +=  `<li class='drink'>
             <p>${data.drinks[i].strDrink}</p>
             <img src="${data.drinks[i].strDrinkThumb}">
@@ -46,9 +77,8 @@ function onRequestHandler() {
 
         drink.forEach((dr, i) => {
             dr.addEventListener('click', () => {
-                console.log(data.drinks[i])
                 const dataDrinksInfo = Object.entries(data.drinks[i])
-
+                scrolls()
                 HTMLResponse.innerHTML = `
                 <div class='drink_description2'>
                 <h2 style="color:white;"> ${data.drinks[i].strDrink}</h2>
@@ -69,21 +99,34 @@ function onRequestHandler() {
                         <p class="drinks_ingredients">
                             ${dataDrinksInfo[i][1]}
                         </p>`
-                        console.log(dataDrinksInfo[i][1])
                     } else if(dataDrinksInfo[i][0].includes('strMeasure') && dataDrinksInfo[i][1] !== null) {
                         ingredientsQuantity.innerHTML += `
                         <p class="drinks_ingredients_amount">
                             ${dataDrinksInfo[i][1]}
                         </p>`
-                        console.log(dataDrinksInfo[i][1])
                     }
                 }
             })
         })
-        console.log(drink)
+        
+
     } 
 }
 
 xhr.addEventListener('load', onRequestHandler);
 xhr.open('GET', `${API_LIST_URL}`)
 xhr.send()
+
+
+
+
+
+function onRequestHandler2() {
+    if(this.readyState == 4 && this.status == 200) {
+        console.log(this.response)
+    }
+}
+
+xhr2.addEventListener('load', onRequestHandler2);
+xhr2.open('GET', `${API_SEARCH}`)
+xhr2.send()
